6ee6ab1f4d3c0fdb6e596c62a3a1b9df012e00b9
Tamimu Said Rashid <121192802+tamimurashid@users.noreply.github.com>
2025-03-08 00:14:47 +0300

    update

--- Api/api.php
+++ Api/api.php
@@ -1,5 +1,5 @@
 <?php 
-require "./server/db.php";
+include"../server/db.php";
 header('Access-Control-Allow-Origin:*');
 header('Content-Type: application/json');
 header('Acess-Control-Allow-Method: GET');
@@ -9,17 +9,30 @@ $rawData = file_get_contents("php://input");
 
 $data =  json_decode($rawData , true);// this return php array and not object
 
-if(isset($data['cardID'])){
-    $res_data = [
-
-
-    ];
+if(isset($data['cardID']) || isset($data['code'])){
+    if($data['code'] == '010'){
+        $res_data =[
+            "status" => 'reg_mod',
+            "code" => '010',
+            "message" =>"Registration on pogress"
+        ];
+        echo json_encode($res_data);
+    }
+    elseif($data['code'] == '009'){
+        $res_data = [
+            "status" => 'Auth_mod',
+            "code" => '009',
+            "message" => "Authentication method on"
+        ];
+    }
 
 }else{
     $res_data = [
         "status" => 'error',
         "code" => '000',
-        "message" => "Id not serted"
+        "message" => "Invalid request: cardID not provided"
     ];
+
+    echo json_encode($res_data);
 
 }
