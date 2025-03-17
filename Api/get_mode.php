afa2cc1358415789cfd764ea0240e4c2d67bccb5
Tamimu Said Rashid <121192802+tamimurashid@users.noreply.github.com>
2025-03-17 13:29:23 +0300

    update

--- Api/get_mode.php
+++ Api/get_mode.php
@@ -1,11 +1,14 @@
 <?php
 session_start();
-require "../db.php";
+require "../server/db.php";
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
+
+$rawData = file_get_contents("php://input");
+$data = json_decode($rawData, true);
 
 $result = $conn->query("SELECT mode FROM device_modes LIMIT 1");
 $row = $result->fetch_assoc();
 
 echo json_encode(["status" => $row ? $row['mode'] : "auth_mod"]);
-?>
+
