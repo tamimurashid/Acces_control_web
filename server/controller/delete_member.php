82abd4ef8abeea261a5699ffe075e9bd0d5cfae4
Tamimu Said Rashid <121192802+tamimurashid@users.noreply.github.com>
2025-04-05 23:21:24 +0300

    update

--- server/controller/delete_member.php
+++ server/controller/delete_member.php
@@ -1,5 +1,21 @@
 <?php
+require "../db.php";
+session_start();
+if(isset($_GET['id'])){
+   $id = intval($_GET['id']);
 
+   $stmt =$conn->prepare("DELETE FROM user_deatils WHERE id = ?");
+   $stmt->bind_param("i", $id);
+   
+
+   if($stmt->execute()){
+        $_SESSION['success'] = "New member has been registered, to return the system to authentication mode click here  ";
+        header("Location: http://localhost:8888/Access_control/register.php");
+        exit();
+   }
+
+
+}
 
 
 
